fb0a04f84183407954654b572963f6b033e0c0a5
muqsith <01muqsith@gmail.com>
2024-02-20 20:28:42 +0700

    test

--- test2.js
+++ test2.js
@@ -1,19 +1,18 @@
-//import puppeteer from 'puppeteer';
 import puppeteer from 'puppeteer-core';
-// import StealthPlugin from 'puppeteer-extra-plugin-stealth';
+import 'dotenv/config';
 
-export const prabowo = async () => {
-  //   puppeteer.use(StealthPlugin());
+const prabowo = async () => {
   const browser = await puppeteer.launch({
-    headless: true,
+    headless: 'new',
+    executablePath: process.env.PATH_CHROME,
   });
   const page = await browser.newPage();
-  await page.goto('https://kawalpemilu.org/');
+  await page.goto('https://ethol.pens.ac.id/');
   await page.waitForSelector(
-    'body > app-root > div > mat-sidenav-container > mat-sidenav-content > app-hierarchy > div > table > tfoot > tr > th:nth-child(3) > app-percent > span:nth-child(1)',
+    '#box-card-services > div > div:nth-child(6) > div > div > div > div.col-sm-12.col-md-9.col-12 > h2',
   );
   const prabowo = await page.$eval(
-    'body > app-root > div > mat-sidenav-container > mat-sidenav-content > app-hierarchy > div > table > tfoot > tr > th:nth-child(3) > app-percent > span:nth-child(1)',
+    '#box-card-services > div > div:nth-child(6) > div > div > div > div.col-sm-12.col-md-9.col-12 > h2',
     (element) => {
       return element.textContent;
     },
@@ -21,41 +20,4 @@ export const prabowo = async () => {
   await page.close();
   return prabowo;
 };
-// export const anis = async () => {
-//   puppeteer.use(StealthPlugin());
-//   const browser = await puppeteer.launch({headless: 'new'});
-//   const page = await browser.newPage();
-//   await page.goto('https://kawalpemilu.org/');
-//   await page.waitForSelector(
-//     'body > app-root > div > mat-sidenav-container > mat-sidenav-content > app-hierarchy > div > table > tfoot > tr > th:nth-child(3) > app-percent > span:nth-child(1)',
-//   );
-//   const anis = await page.$eval(
-//     'body > app-root > div > mat-sidenav-container > mat-sidenav-content > app-hierarchy > div > table > tfoot > tr > th:nth-child(2) > app-percent > span:nth-child(1)',
-//     (element) => {
-//       return element.textContent;
-//     },
-//   );
-//   await page.close();
-//   return anis;
-// };
-// export const ganjar = async () => {
-//   puppeteer.use(StealthPlugin());
-//   const browser = await puppeteer.launch({headless: 'new'});
-//   const page = await browser.newPage();
-//   await page.goto('https://kawalpemilu.org/');
-//   await page.waitForSelector(
-//     'body > app-root > div > mat-sidenav-container > mat-sidenav-content > app-hierarchy > div > table > tfoot > tr > th:nth-child(3) > app-percent > span:nth-child(1)',
-//   );
-//   const ganjar = await page.$eval(
-//     'body > app-root > div > mat-sidenav-container > mat-sidenav-content > app-hierarchy > div > table > tfoot > tr > th:nth-child(4) > app-percent > span:nth-child(1)',
-//     (element) => {
-//       return element.textContent;
-//     },
-//   );
-//   await page.close();
-//   return ganjar;
-// };
-
 console.log(await prabowo());
-// console.log(await anis());
-// console.log(await ganjar());
